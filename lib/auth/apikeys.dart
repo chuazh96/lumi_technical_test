14960ab5dff97c5d821f1637cedfc5e4714545f8
chuazh96 <chua@withes.org>
2023-02-10 10:28:09 +0800

    api keys added to repo

---
+++ lib/auth/apikeys.dart
@@ -0,0 +1,14 @@
+const gsheetsKey = r''' {
+  "type": "service_account",
+  "project_id": "lumitest",
+  "private_key_id": "0ea8dbc18e749b0f67424d02c6af40ea7d3e2eb9",
+  "private_key":
+      "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCe+evDoomhzBjW\npeguS+YTkgF8oQEJWdhHeCScNpQQqZNl4c73YB6Ip7dfWC/3qJIVuYWup3RyOcfg\nMSQzvJ9SrqJKwr2yBt8n5BhAKC0OEhU4NRHfKVzSXTFByBrQKrOaXlh7BQkgUfDj\nZAelfqbGTrw5sTiZ7jNzjoz3yNS61vZ43tkE4tHRa+dRECmOgyU6tKzCy70sL8Mv\n00h0Wx4aWLNy7DkF0T5hzyiN3LARnT8ycZ6ciPyeAHpvrUYTKywE2ukiPpsXjMWJ\nhZHJT99eMDfQl10yYe8SytoLjpl95w8y31kAozztBXf1PF2IO9Uo2Dj+WDSUBqPr\nOh/VzEyRAgMBAAECggEAAiZajhd3WYEHT/XH5UJg/uBBVWRe/Xeh0VuaUSN6zpvg\ncqcCU72QxCPSer9YTLbSnOkYmfNJQ5cs8QbxGKMDG4zhhHXMN2lY4LL4sftpGpDb\niRpMntIyH2UjuqJAXJLLQXnno+g+RpndElIiuVX0XuenK3eOTqSWPQiPNvWYDNvA\nXhUi3FL3hg9/sI0uQzt3VISfBnxUFIVdP/eBpeenvwI31I/lgdLCi/M/xdJLz11D\nZl4Ol27DGGFkVqIxuKz9x8ui5u7ZcGmW/cNQP4zt3JLWzlfXN4agfXJ6JrcsWXNJ\nI+Bq5NTjDlO8IsNgu8F7FL6Xd44qLuTrxKiB5KkTXQKBgQDMlfnjZ8Qk6DYsS5KY\numMh+3poHBY1uGYD54A6HJ9vy3JJwd0B9bIYeLA+magY3unE6O4/4eEFpfg7hXSU\n2VH9aFm997HLRn3IZG2cEMGhgI+/qJmW0vf0JMinaIhXe6hnu3v8QB3gP4Eda6f7\nVlGwg1WExAGCfTCYp8Zzvgv2DQKBgQDG7ac7h1xj9238Seo62K5BygmnxUxLMmC9\n0JdWcDLKfyZMALf1yjaVRteAbaZvShPOvYB+SjA5p19YZjRBw7dl/TlCHEBNLWmS\n0M24YMOPrxATStQXAKRAqtvS0yuAcEKsCotc+At/ImuYSAFIqilzOREfNQdoV12d\nnoG9OzezlQKBgErbpa/bzB1lCk1y6ZUQvi01AzkM7eOCMXHER1r8Fiql2o+6a8/H\nwv7JSlPdjXBsQEVYpbSiRd/3Nrkwv9OnISUEBbJI67lhdazZcnHkf7VEgVSzsF9L\nmoau3uzChwDbsmciZt+J5mSjV+8X8UM59nIL3ELUJWlFvX5eBXNcWqwRAoGASLic\nSADF362WQYJr5DNOELLhMLnJmDvEbd+RU4IUrz4oK+SfW3Ue2xdYtJRhIZH9B7jy\n/jf0xT7Qdip2tAvuQZR+mBJn107gp1E5w0tsclxXDhZuzjHn2dQcJEgaQQvI88ev\nBr+OxF065CJ+haffG0YjYjsqiBRYHsTRPPIm/LkCgYBnxG1VYkLAeQYQ/9Cv80Gr\n8ZsRLLa/jfP4HV3ZsKLTwg8VPHzmlDL9yP4y85KJob7lmALZbKdooBhfhvvoOkcE\nyhqe0x99eTZx/UckLzvyVsNoxHP/4ie6QRZTRldP9b/Hy8uApJAwofRm1pdB5weO\ne50B0/ikXYVC6mfSDik/Bg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "gsheets@lumitest.iam.gserviceaccount.com",
+  "client_id": "100241960971637181037",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url":
+      "https://www.googleapis.com/robot/v1/metadata/x509/gsheets%40lumitest.iam.gserviceaccount.com"
+}''';
